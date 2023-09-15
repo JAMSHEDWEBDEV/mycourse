212b9831f0ee7bfb7a432005727c2fc77f1c8bcb
JAMSHEDWEBDEV <jamsheddc60@gmail.com>
2023-09-16 02:06:56 +0600

    added All course container component

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Allcourse from './components/Allcourse/Allcourse'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Allcourse></Allcourse>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Selectcourse/Cart.jsx
@@ -0,0 +1,10 @@
+
+const Cart = () => {
+    return (
+        <div>
+            <h1>this is cart section</h1>
+        </div>
+    );
+};
+
+export default Cart;
